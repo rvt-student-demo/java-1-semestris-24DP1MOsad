e18dc61ea32c233066f1cb7417fb532518a477bf
24DP1MOsad <a240503mo@rvt.lv>
2025-09-18 12:38:08 +0300

    Grades and Points

---
+++ src/main/java/rvt/Grades_and_Points.java
@@ -0,0 +1,29 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Grades_and_Points {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give points [0-100]");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number < 0) {
+            System.out.println("Grade: impossible!");
+        } else if (number < 50) {
+            System.out.println("Grade: failed");
+        } else if (number < 60) {
+            System.out.println("Grade: 1");
+        } else if (number < 70) {
+            System.out.println("Grade: 2");
+        }  else if (number < 80) {
+            System.out.println("Grade: 3");
+        } else if (number < 90) {
+            System.out.println("Grade: 4");
+        }    else if (number <= 100) {
+            System.out.println("Grade: 5");
+        } else if (number > 100) {
+            System.out.println("Grade: impossible!");
+        }
+        scanner.close();
+    }
+}
