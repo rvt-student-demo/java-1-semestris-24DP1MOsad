4caa1c3ad9d1c8ad0a2bcde620560b7c1f549e31
24DP1MOsad <a240503mo@rvt.lv>
2025-09-24 08:55:17 +0300

    Gift tax

---
+++ src/main/java/rvt/Gift_tax.java
@@ -0,0 +1,25 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Gift_tax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Value of the gift?");
+        int gift = Integer.valueOf(scanner.nextLine());
+        scanner.close();
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift < 25000) {
+            System.out.println("Tax: " + (100 + (gift - 5000) * 0.08));
+        } else if (gift < 55000) {
+            System.out.println("Tax: " + (1700 + (gift - 25000) * 0.1));
+        } else if (gift < 200000) {
+            System.out.println("Tax: " + (4700 + (gift - 55000) * 0.12));
+        } else if (gift < 1000000) {
+            System.out.println("Tax: " + (22100 + (gift - 200000) * 0.15));
+        } else {
+            System.out.println("Tax: " + (142100 + (gift - 1000000) * 0.17));
+        }
+    }
+}
