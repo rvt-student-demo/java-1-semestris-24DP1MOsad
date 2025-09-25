0e12952d058104951cfd4731f094f11e62e6c67c
24DP1MOsad <a240503mo@rvt.lv>
2025-09-25 13:01:12 +0300

    number and sum of numbers

---
+++ src/main/java/rvt/number_and_sum_of_number.java
@@ -0,0 +1,21 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class number_and_sum_of_number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int sk = 0;
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scanner.nextLine());
+        while (num != 0) {
+            sum += num;
+            sk += 1;
+            System.out.println("Give a number:");
+            num = Integer.valueOf(scanner.nextLine());
+        }
+        System.out.println("Number of numbers: " + sk);
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
